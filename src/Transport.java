f6394c8a44d8e3d75aec2642fa7e6071a9b1b0c6
Мамбеткеримов Данияр Сарлык уулу <mdaniyar1995@gmail.com>
2022-02-04 10:12:33 +0300

    Initial commit

---
+++ src/Transport.java
@@ -0,0 +1,10 @@
+public abstract class Transport {
+    int fuel, speed;
+
+    public Transport(int fuel, int speed) {
+        this.fuel = fuel;
+        this.speed = speed;
+    }
+    public abstract boolean  canMove(int n);
+    public abstract int getFuelLevel();
+}
